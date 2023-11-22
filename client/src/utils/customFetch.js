14b2fd39b091095bbb598001a8aa81cfa5aec929
Shubhangam333 <shubhangam27@gmail.com>
2023-11-22 17:26:56 +0530

    fixed some bugs

--- client/src/utils/customFetch.js
+++ client/src/utils/customFetch.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 const config = {
   headers: { "Content-Type": "application/json" },
-  withCredentials: true,
+  // withCredentials: true,
 };
 const customFetch = axios.create({
-  baseURL: "https://delight-shop-eta.vercel.app",
+  baseURL: "/api/v1",
   config,
 });
 
